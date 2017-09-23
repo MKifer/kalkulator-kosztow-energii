a08bc9c42c8747684a591c7d6911fabcd5001c34
KEFF <maciej.kifer@gmail.com>
2017-09-23 14:48:13 +0200

    adding some jquery

---
+++ resources/js/introscript.js
@@ -0,0 +1,10 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+        var options = {
+      strings: ["Cześć", "Czy chciałbyś szybko policzyć ile kosztuje prąd jaki pobierają Twoje urządzenia ?", "Oto szybki i prosty kalkulator kosztów energii"],
+      typeSpeed: 40
+    }
+
+    var typed = new Typed(".intro-textbox h1", options);
+});
+
